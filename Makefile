72924a24fb19c87f96890bd69dc3eee823a3c715
Julian Pritzi <julian.pritzi@gmail.com>
2022-11-28 16:38:52 +0000

    working commit

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ default: all
 all: proofs/secure_boot_analyzed.spthy proofs/hardware_attestation_analyzed.spthy
 
 proofs/secure_boot_analyzed.spthy: secure_boot.spthy 
-	tamarin-prover --prove secure_boot.spthy -O=proofs
+	tamarin-prover --prove secure_boot.spthy -O=proofs > proofs/secure_boot_summary.txt
 
-proofs/hardware_attestation_analyzed.spthy: hardware_attestation.spthy 
-	tamarin-prover --prove hardware_attestation.spthy -O=proofs
+proofs/hardware_attestation_analyzed.spthy: hardware_attestation.spthy hardware_attestation_oracle.py
+	tamarin-prover --prove hardware_attestation.spthy -O=proofs > proofs/hardware_attestation_summary.txt
--- hardware_attestation_oracle.py
+++ hardware_attestation_oracle.py
@@ -27,6 +27,24 @@ for line in lines:
       l3.append(num)
     else:
       l4.append(num)
+      
+  elif lemma in ["kernel_trust_means_icu_done", "icu_trust_means_kernel_done"]:
+    
+    if ": !KU( ~dh_" in line \
+        or ": !KU( common_key" in line:
+      l1.append(num)
+    elif ": !KU(" in line and not ("sign" in line) and (
+          re.search(r".*\^.*inv", line) or # x ^ ( inv(y) )
+          re.search(r".*\^\(.*(priv.*\*|\*.*priv)", line) # x ^ ( priv * ... )
+        ):
+      l2.append(num)
+    elif ": !KU(" in line and (
+          ("sign" in line and "ca_priv" in line) or
+          "senc" in line
+        ):
+      l3.append(num)
+    else:
+      l4.append(num)
   else:
     exit(0)
 
